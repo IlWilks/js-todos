3d9e8c9e250870bc352dcd092f926bde59106a72
Ian <ianlloydwilkinson@gmail.com>
2020-12-03 14:44:01 -0700

    Showing/hiding list and completed tasks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 let state = {
   tasks: [],
+  rendered: false,
 }
 
 axios
@@ -23,22 +24,49 @@ const renderTask = (task) => {
           <p>Completed: ${task.completed}</p>`
 
 }
+const clickToRender = () => {
+  state.rendered = true;
+  render();
+}
 
+const clickToUnRender = () => {
+  state.rendered = false;
+  render();
+}
 const renderList = () => {
   const {tasks} = state
-  console.log(state.tasks)
-  let tasksArray = tasks.map(task => {
-    return renderTask(task)
-  })
 
-  return `<div class = "grid">${tasksArray.join(" ")}</div>`
+  if (state.rendered == true) {
+    let tasksArray = tasks.map(task => {
+      return renderTask(task)
+    })
+
+    return `<div class = "grid">${tasksArray.join(" ")}</div>`
+  }
+  return ""
+
+  render();
+}
+
+const hideCompleted = () => {
+  if (state.rendered == true) {
+    console.log('hide test')
+    let filteredCompleted = state.tasks.filter(task => task.completed !== true)
+    state.tasks = filteredCompleted;
+    render();
+  }
+  return ""
 }
 
 
   const render = () => {
     let htmlString = `<div>`
     htmlString += `<h1>To-do</h1>`
-    htmlString += `<div class="Render" onclick = "renderList()">Show List</div>`
+    htmlString +=  `<div class = "list-controls">`
+    htmlString +=   `<button class="buttons" onclick = clickToRender()>Show List</button>`
+    htmlString +=   `<button class="buttons" onclick = clickToUnRender()>Hide List</button>`
+    htmlString +=   `<button class="buttons" onclick = hideCompleted()>Hide Completed Tasks</button>`
+    htmlString +=  `</div>`
     htmlString += renderList();
     document.getElementById('app').innerHTML = htmlString
   }
